4e9000a3ca9830df578ec8f844bd52b543204071
Sven Otto <svenotto@uw.edu>
2020-08-19 17:34:22 -0700

    initial commit after class, 4 of 5 tests failing

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 const { Router } = require('express');
 const router = Router();
 
+router.get("/", (req, res, next) => {
+    const timeStamp = new Date().toTimeString();
+    res.render('index', { items: ['item one', 'other', 'new item'] });
+});
 
 module.exports = router;
